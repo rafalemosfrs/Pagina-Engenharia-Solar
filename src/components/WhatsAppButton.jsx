b8bdf5279bc683182ccfa63da3954d71b9eb9e13
Rafael Lemos <rafaelemosfr@gmail.com>
2025-02-16 08:57:12 -0300

    Adição de funcionalidades whatsapp

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -4,7 +4,7 @@ import { MessageCircle } from 'lucide-react';
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/+5585991347203"
+      href="https://wa.me/+5585991347203/?text=Ol%C3%A1,%20gostaria%20de%20um%20or%C3%A7amento."
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition-colors z-50 flex items-center justify-center"
